cffcfa11177a64932aa595edaca4ad376a32c286
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2018-08-13 11:22:45 +0200

    Move package files

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Golang SDK for the [puppet-master.io](https://puppet-master.io) public API. Puppet-master makes the execution of website interactions
+// super simple by abstracting the code execution behind a HTTP API, scheduling the job for you in a scalable
+// manner. For more information please head over to the [puppet-master docs](https://docs.puppet-master.io).
+package puppet_master
